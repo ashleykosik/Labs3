b37381a79b4faa138435ce8440dbececf8a2c52e
Ashley Kosik <ashley.kosik@gmail.com>
2023-04-18 20:28:44 -0400

    Day 12 text & shapes in css

---
+++ challenge-stock-prices/stockPrices.js
@@ -0,0 +1,21 @@
+
+let a = [1, 2, 3, 4, 5]
+let b = [100, 50, 20]
+let c = [5, 2, 7, 9]
+
+function best(num) {
+    let min =[]   
+    
+    //is the first number not the max / is this the max
+   for(i = 1; i < num.length; i++)  {
+    if (num[0] > num[i]) {
+       return console.log('$0')
+    } else if (num[0] < num[i]) {
+        return num[0] - num[i]
+    }
+    
+    //find min
+}
+}
+
+// if the first number is lower
